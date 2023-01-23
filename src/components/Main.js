e0ac4471b15e1f647276251461dfb132ecbe029e
kidavito <kidavito.user@gmail.com>
2023-01-24 05:00:22 +0700

    added: pages, react-router-dom & navigation link

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,26 @@
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Menu from "../pages/Menu";
+import Reservations from "../pages/Reservations";
+import OrderOnline from "../pages/OrderOnline";
+import Login from "../pages/Login";
+import { Routes, Route } from "react-router-dom";
+
 function Main() {
-    return <div></div>
+  return <main>
+      <section>
+            <article>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/menu" element={<Menu />}></Route>
+                <Route path="/reservations" element={<Reservations />}></Route>
+                <Route path="/order-online" element={<OrderOnline />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+              </Routes>
+            </article>
+        </section>
+  </main>;
 }
 
 export default Main;
---
+++ src/pages/OrderOnline.js
@@ -0,0 +1,5 @@
+function OrderOnline() {
+    return <h1>This is "Order Online" page.</h1>
+}
+
+export default OrderOnline;
