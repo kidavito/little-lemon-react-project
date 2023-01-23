6bff53c7dc7cd25be72bd6e9cbc908758e3e7f93
kidavito <kidavito.user@gmail.com>
2023-01-24 04:37:07 +0700

    the footer logo was set

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
+import footer_logo from '../assets/img/little_lemon_logo.png';
+
 function Footer() {
-    return <div></div>
+    return <footer>
+        <div>
+            <img src={footer_logo} height={50} alt="Little Lemon Footer Logo" />
+        </div>
+        <div>
+            <p>Copyright © 2023 Little Lemon</p>
+        </div>
+    </footer>;
 }
 
 export default Footer;
