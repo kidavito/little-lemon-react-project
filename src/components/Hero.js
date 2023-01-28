338350d9779ba8d0514d5db4e4e6c02adfb384d9
kidavito <kidavito.user@gmail.com>
2023-01-28 20:47:41 +0700

    unecessary classes removed from Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,9 +7,9 @@ export default function Hero() {
         <section id="hero">
             <article className="hero-container">
                 <div className="hero-text-container">
-                    <h1 className="hero-title">Little Lemon</h1>
-                    <h2 className="hero-locale">Chicago</h2>
-                    <p className="hero-description">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                     <ButtonGoldGlow link="/reservations" text="Reserve a Table" />
                 </div>
                 <img src={hero_image} className="hero-image" alt="hero" />
