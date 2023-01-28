96997e9a41c93b8aba98d0aee374395048060dd5
kidavito <kidavito.user@gmail.com>
2023-01-28 20:51:16 +0700

    unecessary class removed from Promo component

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -4,7 +4,7 @@ import ButtonGoldGlow from "./ButtonGoldGlow";
 export default function Promo() {
     return (
         <section id="promo">
-            <h2 className="promo-title">This weeks specials!</h2>
+            <h2>This weeks specials!</h2>
             <ButtonGoldGlow link="/order-online" text="Online Menu" />
         </section>
     )
