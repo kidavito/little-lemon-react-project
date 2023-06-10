fe922eb9b351ea08ef767d8f104a6631983a77ab
kidavito <kidavito.user@gmail.com>
2023-06-10 21:16:52 +0700

    links updated

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Menu() {
-    const textStyle = {
-        textAlign: "center",
-    }
-    const linkStyle = {
-        color:  "black",
-    }
-    return (
-        <>
-            <h1 style={textStyle}>This "Menu" page is under construction.</h1>
-            <p style={textStyle}>Please visit <Link to="/reservations" style={linkStyle}>Reservations</Link> page to book a table.</p>
-        </>
-    );
+  const textStyle = {
+    textAlign: "center",
+  };
+  const linkStyle = {
+    color: "black",
+  };
+  return (
+    <>
+      <h1 style={textStyle}>This "Menu" page is under construction.</h1>
+      <p style={textStyle}>
+        Please visit{" "}
+        <Link to="/little-lemon-react-project/reservations" style={linkStyle}>
+          Reservations
+        </Link>{" "}
+        page to book a table.
+      </p>
+    </>
+  );
 }
