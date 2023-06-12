52f0a9eb7bf67d0246f1ee14200ebe45adc27d22
kidavito <kidavito.user@gmail.com>
2023-06-12 20:40:21 +0700

    fixed link to

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Login() {
-    const textStyle = {
-        textAlign: "center",
-    }
-    const linkStyle = {
-        color:  "black",
-    }
-    return (
-        <>
-            <h1 style={textStyle}>This "Login" page is under construction.</h1>
-            <p style={textStyle}>Please visit <Link to="/reservations" style={linkStyle}>Reservations</Link> page to book a table.</p>
-        </>
-    )
+  const textStyle = {
+    textAlign: "center",
+  };
+  const linkStyle = {
+    color: "black",
+  };
+  return (
+    <>
+      <h1 style={textStyle}>This "Login" page is under construction.</h1>
+      <p style={textStyle}>
+        Please visit{" "}
+        <Link to="/little-lemon-react-project/reservations" style={linkStyle}>
+          Reservations
+        </Link>{" "}
+        page to book a table.
+      </p>
+    </>
+  );
 }
