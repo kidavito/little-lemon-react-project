d3402a957e825b6c3d9c97a9fa6cdca3c30a8c1e
kidavito <kidavito.user@gmail.com>
2023-06-13 06:04:05 +0700

    name updated

--- src/components/primary/Footer.js
+++ src/components/primary/Footer.js
@@ -1,20 +1,26 @@
-import './Footer.css';
+import "./Footer.css";
 // import ThemeManager from '../ThemeManager';
-import footer_logo from '../../assets/img/little_lemon_logo.png';
+import footer_logo from "../../assets/img/little_lemon_logo.png";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-    return (
-        <footer >
-            <section className='footer-container'>
-                {/* <ThemeManager /> */}
-                <article>
-                    <Link to="/">
-                        <img src={footer_logo} height={50} alt="Little Lemon Footer Logo" />
-                    </Link>
-                    <p>Copyright &copy; 2023 Little Lemon | Coded by <a href="https://github.com/kidavito" target="blank" id="coder">David Andianto</a>.</p>
-                </article>
-            </section>
-        </footer>
-    )
+  return (
+    <footer>
+      <section className="footer-container">
+        {/* <ThemeManager /> */}
+        <article>
+          <Link to="/">
+            <img src={footer_logo} height={50} alt="Little Lemon Footer Logo" />
+          </Link>
+          <p>
+            Copyright &copy; 2023 Little Lemon | Coded by{" "}
+            <a href="https://github.com/kidavito" target="blank" id="coder">
+              Ki David Andianto
+            </a>
+            .
+          </p>
+        </article>
+      </section>
+    </footer>
+  );
 }
