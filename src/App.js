6de57baa356a492f648ecf96c74f7718aa8f0ce6
kidavito <kidavito.user@gmail.com>
2023-06-12 20:38:52 +0700

    optimized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import Header from "./components/primary/Header";
 import Nav from "./components/primary/Nav";
 import Main from "./components/primary/Main";
@@ -7,13 +9,13 @@ import Footer from "./components/primary/Footer";
 
 export default function App() {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <div className="page-top">
         <Header />
         <Nav />
       </div>
       <Main />
       <Footer />
-    </React.Fragment>
+    </BrowserRouter>
   );
 }
