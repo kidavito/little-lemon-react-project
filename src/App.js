4cb7a180b0aeb1469b2f02f7bcd1a5bef4c9396f
kidavito <kidavito.user@gmail.com>
2023-06-14 05:15:13 +0700

    credit added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Main from "./components/primary/Main";
 import Footer from "./components/primary/Footer";
 
 export default function App() {
+  console.log(
+    `Coded by "Ki David Andianto", more at: https://github.com/kidavito`
+  );
   return (
     <BrowserRouter>
       <div className="navbar-container">
