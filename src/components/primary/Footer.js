7ed1d027aaca41502b391a96b3e22a001fa6e446
kidavito <kidavito.user@gmail.com>
2023-06-14 23:27:24 +0700

    automatically change year text

--- src/components/primary/Footer.js
+++ src/components/primary/Footer.js
@@ -4,6 +4,7 @@ import footer_logo from "../../assets/img/little_lemon_logo.png";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer>
       <section className="footer-container">
@@ -13,7 +14,7 @@ export default function Footer() {
             <img src={footer_logo} height={50} alt="Little Lemon Footer Logo" />
           </Link>
           <p>
-            Copyright &copy; 2023 Little Lemon | Coded by{" "}
+            Copyright &copy; {year} Little Lemon | Coded by{" "}
             <a href="https://github.com/kidavito" target="blank" id="coder">
               Ki David Andianto
             </a>
